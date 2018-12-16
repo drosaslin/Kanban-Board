c38594bc816941760117101499a8abfa48dcbf29
daniel <ahm12345@gmail.com>
2018-12-16 12:15:00 +0800

    新增彈跳視窗

--- kanbanapp/src/app/components/homepage/homepage.component.spec.ts
+++ kanbanapp/src/app/components/homepage/homepage.component.spec.ts
@@ -1,7 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomepageComponent } from './homepage.component';
-
 describe('HomepageComponent', () => {
   let component: HomepageComponent;
   let fixture: ComponentFixture<HomepageComponent>;
