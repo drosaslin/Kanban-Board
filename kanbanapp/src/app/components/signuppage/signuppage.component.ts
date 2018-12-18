838ac6e81391337638e8ced55f8d68ee866fb7e2
genesisyau <genesisyau@hotmail.com>
2018-12-18 14:15:55 +0800

    added sign up w/email & password

--- kanbanapp/src/app/components/signuppage/signuppage.component.ts
+++ kanbanapp/src/app/components/signuppage/signuppage.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { Router } from '@angular/router';
+import { AngularFirebaseDatabase, AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs';
 import * as firebase from 'firebase/app';
 
 @Component({
@@ -8,10 +11,29 @@ import * as firebase from 'firebase/app';
   styleUrls: ['./signuppage.component.css']
 })
 export class SignuppageComponent implements OnInit {
+  firstName: string;
+  lastName: string;
+  username: string;
+  email: string;
+  password1: string;
+  password2: string;
 
-  constructor(public afAuth: AngularFireAuth) {}
+  constructor(public afAuth: AngularFireAuth,
+    private router: Router,
+    private db: AngularFireDatabase) { }
 
   ngOnInit() {
+  }
+
+  signUpClick(): void {
+    if (this.password1 === this.password2) {
+      this.afAuth.auth.createUserWithEmailAndPassword(this.email, this.password1)
+        .then((res) => {
+          this.router.navigate(['']);
+        });
+
+    }
+
   }
 
 }
