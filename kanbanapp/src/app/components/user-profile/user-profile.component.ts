28a38c4351ed3d86c43b79ed9dd80cfb5b8fd0ec
drosaslin <davidrosas.xc@gmail.com>
2018-12-19 22:47:57 +0800

    modifications to user profile page

--- kanbanapp/src/app/components/user-profile/user-profile.component.ts
+++ kanbanapp/src/app/components/user-profile/user-profile.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
+  private readonly editText = 'Edit';
+  private readonly saveText = 'Save';
+
   private firstName: string;
   private lastName: string;
   private userName: string;
@@ -23,13 +26,21 @@ export class UserProfileComponent implements OnInit {
     this.userName = 'Cuyo';
     this.email = 'cuy@cuy.com';
     this.password = '**********';
-    this.editButtonText = 'Edit';
+    this.editButtonText = this.editText;
     this.isEditProfileEnabled = false;
   }
 
   public editProfileClick(): void {
     this.isEditProfileEnabled = !this.isEditProfileEnabled;
+    this.editButtonText = (this.isEditProfileEnabled) ? this.saveText : this.editText;
+  }
 
-    this.editButtonText = (this.isEditProfileEnabled) ? 'Save' : 'Edit';
+  public cancelButtonClick(): void {
+    this.disableEditProfile();
+  }
+
+  private disableEditProfile(): void {
+    this.isEditProfileEnabled = false;
+    this.editButtonText = this.editText;
   }
 }
