1e65a3b27317b03f5882759409df98a99c0ba2dc
genesisyau <genesisyau@hotmail.com>
2018-12-22 21:12:22 +0800

    Homepage now displays the user groups

--- kanbanapp/src/app/kanban-model/model.ts
+++ kanbanapp/src/app/kanban-model/model.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { User } from './classes/user';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 // @Injectable()
 export class KanbanModel {
+    db: any;
+    afAuth: any;
 
-    constructor() {
+    constructor(db: AngularFireDatabase, afAuth: AngularFireAuth) {
     }
+
+    // public loadUserData(): any {
+    //     let user$: any;
+
+        // this.db.object('users/' + this.afAuth.auth.currentUser.uid).valueChanges()
+        //     .subscribe(user => {
+        //         user$ = user;
+        //         console.log(user$);
+        //     });
+    // }
 }
